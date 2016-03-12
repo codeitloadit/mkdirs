335cb48cab6485283162525f29916287380b2b05
codeitloadit <brian@codeitload.it>
2016-03-11 16:47:58 -0800

    Mostly working.

--- mkdirs/main.go
+++ mkdirs/main.go
@@ -2,8 +2,53 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
-	fmt.Println("")
+	var err error
+	var path string
+	var perm uint64
+	var strPerm string = "755" // Default for mkdir
+
+	args := os.Args[1:]
+
+	switch len(args) {
+	case 0:
+		showUsage()
+	case 1:
+		path = args[0]
+	case 2:
+		path = args[0]
+		strPerm = args[1]
+		if len(strPerm) > 4 {
+			showUsage()
+		}
+	default:
+		fmt.Println("Unexpected arguments: ", args[2:])
+		showUsage()
+	}
+
+	for _, arg := range args {
+		if arg == "-h" || arg == "--help" {
+			showUsage()
+		}
+	}
+
+	perm, err = strconv.ParseUint(strPerm, 8, 32)
+	fmt.Println(perm)
+	if err != nil {
+		fmt.Printf("perm [%v] must be an integer [0...7777]", args[1])
+	}
+
+	err = os.MkdirAll(path, os.FileMode(perm))
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func showUsage() {
+	fmt.Println("usage: mkdirs path [perm]")
+	os.Exit(0)
 }
